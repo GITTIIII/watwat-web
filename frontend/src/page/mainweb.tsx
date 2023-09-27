d37deb119475052a5759bde05e310787774b8cc0
[username] <[user>
2023-09-28 02:47:22 +0700

    w

--- frontend/src/page/mainweb.tsx
+++ frontend/src/page/mainweb.tsx
@@ -1,31 +1,56 @@
 import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
-import { faMagnifyingGlass } from "@fortawesome/free-solid-svg-icons"; //for icon
-import { Link } from "react-router-dom";
+import { faMagnifyingGlass, faPlay } from "@fortawesome/free-solid-svg-icons"; //for icon
 import "../css/mainweb.css";
 import Footer from "../component/footer";
 import NavbarSearch from "../component/navbar-search";
-import NavbarMain from "../component/navbar-main";
+import React from "react";
 const MainWeb = () => {
-  
+  const [searchclick,setSearchClick] = React.useState(false)
+
   return (
     <>
       <header>
         <NavbarSearch/>
       </header>
       <main style={{ backgroundImage: "url(/image/temple.jpg)" }}>
-        <div className="middle-box">
-          <div>WAT WAT "ยินดีต้อนรับ"</div>
+        <div className={`middle-box ${searchclick ? "active" : ""}`}>
+          <span>WAT WAT "ยินดีต้อนรับ"</span>
           <div className="search-bar">
             <FontAwesomeIcon icon={faMagnifyingGlass} className="icon" />
             <input type="text" placeholder="ค้นหาวัด" />
-              <button className="search-button">
-                  <Link to="/place">ค้นหา</Link>
+              <button className="search-button" onClick={() => {setSearchClick(true)}}>
+                  ค้นหา
               </button>
           </div>
         </div>
-        <div className="show-result-box">
-          
-        </div>
+        {searchclick && <div className="show-result-box">
+          <div className="wat-box">
+            <div className="wat">
+              
+            </div>
+            <div className="wat">
+              
+            </div>
+            <div className="wat">
+              
+            </div>
+            <div className="wat">
+              
+            </div>
+            <div className="wat">
+              
+            </div>
+          </div>
+          <div className="pagination">
+              <FontAwesomeIcon icon={faPlay} rotation={180} className="icon"/>
+              <div className="page-select">1</div>
+              <div>2</div>
+              <div>3</div>
+              <div>4</div>
+              <div>5</div>
+              <FontAwesomeIcon icon={faPlay} className="icon"/>
+          </div>
+        </div>}
       </main>
       <footer>
         <Footer/>
