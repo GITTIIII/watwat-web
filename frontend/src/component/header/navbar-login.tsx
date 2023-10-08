a7b8bc741d16ba08e5bde35876ccac19211dd4af
Paultan51 <nathaphon.swd@gmail.com>
2023-10-09 00:09:25 +0700

    what2

---
+++ frontend/src/component/header/navbar-login.tsx
@@ -0,0 +1,17 @@
+import "./navbar.css";
+import Logo from "../logo/logo";
+
+function NavbarLogin() {
+  return (
+    <>
+      <div className="navbar">
+        <div className="topleft-navbar">
+          <Logo />
+          <span>Wat Wat</span>
+        </div>
+      </div>
+    </>
+  );
+};
+
+export default NavbarLogin;
