2a8ec8a2d2157e8c13a39fedc3f20a838fc49617
BHU23 <ttthisriton29@gmail.com>
2023-10-09 12:13:26 +0700

    ชื่อ navbar พล

--- frontend/src/component/navbar/navbarCreator.tsx
+++ frontend/src/component/navbar/navbarCreator.tsx
@@ -8,8 +8,8 @@ export default function NavbarCreator() {
             <li className='manu-1'><NavLink to="./main"><span>หน้าหลัก</span></NavLink></li>
             <li className='manu-2'><NavLink to="./addEvent"><span>แจ้งจัดกิจกรรม</span></NavLink></li>
             <li className='manu-3'><NavLink to="./addItemLoan"><span>แจ้งใช้สถานที่</span></NavLink></li>
-            <li className='manu-4'><NavLink to="./addPlacUse"><span>แจ้งใช้สิ่ง/คืนสิ่งของ</span></NavLink></li>
-            <li className='manu-5'><NavLink to="./ceratrWatData"><span>สร้างข้อมูลวัด</span></NavLink></li>
+            <li className='manu-4'><NavLink to="./addPlacUse"><span>แจ้งใช้สิ่ง/เลิกใช้สิ่งของ</span></NavLink></li>
+            <li className='manu-5'><NavLink to="./ceratrWatData"><span>จัดการข้อมูลวัด</span></NavLink></li>
         </ul>
     </div>
   )
