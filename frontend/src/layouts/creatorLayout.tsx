ee69a86e56a5dd86a66b47ae1b202471fb763aad
Paultan51 <nathaphon.swd@gmail.com>
2023-10-09 11:27:50 +0700

    monday3

--- frontend/src/layouts/creatorLayout.tsx
+++ frontend/src/layouts/creatorLayout.tsx
@@ -1,29 +1,24 @@
 import { Outlet } from "react-router-dom";
 import Footer from "../component/footer/footer";
-<<<<<<< HEAD
-import NavbarUser from "../component/header/navbar-user";
+// import NavbarUser from "../component/header/navbar-user";
 import temple from "../assets/temple.jpg";
 import CreatorNavbar from "../component/CreatorNavbar/CreatorNavbar";
-=======
 
 import SidebarCreater from "../component/sidebar/sidebarCreater";
 import HeaderCreator from "../component/header/header-creator";
 import "./creatorLayout.css";
 
->>>>>>> 631fbcc521ee104277c834c1a369bdd046905370
 export default function CreatorLayout() {
   return (
     <>
       <header>
-<<<<<<< HEAD
         <CreatorNavbar />
       </header>
       <main style={{ backgroundImage: `url(${temple})` }}>
         <Outlet />
       </main>
-=======
-        <HeaderCreator />
-      </header>
+      <HeaderCreator />
+
       <div className="warpperAddEvent">
         <div className="sidebarCreater">
           <SidebarCreater></SidebarCreater>
@@ -32,7 +27,6 @@ export default function CreatorLayout() {
           <Outlet />
         </div>
       </div>
->>>>>>> 631fbcc521ee104277c834c1a369bdd046905370
       <footer>
         <Footer />
       </footer>
