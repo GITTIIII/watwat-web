9ebdfe8fa6ba48769026c0aad7e241bad8e852dd
Paultan51 <nathaphon.swd@gmail.com>
2023-10-11 17:55:35 +0700

    wednesday4

--- frontend/src/component/footer/footer.tsx
+++ frontend/src/component/footer/footer.tsx
@@ -1,19 +1,32 @@
-import './footer.css';
+import "./footer.css";
 import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
-import { faFacebook, faXTwitter,faInstagram,faGithub} from "@fortawesome/free-brands-svg-icons";
+import {
+  faFacebook,
+  faXTwitter,
+  faInstagram,
+  faGithub,
+} from "@fortawesome/free-brands-svg-icons";
 function Footer() {
   return (
     <footer>
-        <div className="contact">CONTACT</div>
-        <div>111/3, Surswithee 1, Suranaree </div>
-        <div>Mueang Nakhon Ratchasima, Nakhon Ratchasima, 3000</div>
-        <div>SA_G04@sut.ac.th</div>
-        <div className="iconall">
-          <a href=""><FontAwesomeIcon icon={faFacebook} className="icon 1"/></a>
-          <a href=""><FontAwesomeIcon icon={faXTwitter} className="icon 2"/></a>
-          <a href=""><FontAwesomeIcon icon={faInstagram} className="icon 3"/></a>
-          <a href="https://github.com/BHU23"><FontAwesomeIcon icon={faGithub} className="icon 4"/></a>
-        </div>
+      <div className="contact">CONTACT</div>
+      <div>111/3, Surswithee 1, Suranaree </div>
+      <div>Mueang Nakhon Ratchasima, Nakhon Ratchasima, 30000</div>
+      <div>SA_G04@sut.ac.th</div>
+      <div className="iconall">
+        <a href="https://github.com/BHU23">
+          <FontAwesomeIcon icon={faFacebook} className="icon 1" />
+        </a>
+        <a href="https://github.com/BHU23">
+          <FontAwesomeIcon icon={faXTwitter} className="icon 2" />
+        </a>
+        <a href="https://github.com/BHU23">
+          <FontAwesomeIcon icon={faInstagram} className="icon 3" />
+        </a>
+        <a href="https://github.com/BHU23">
+          <FontAwesomeIcon icon={faGithub} className="icon 4" />
+        </a>
+      </div>
     </footer>
   );
 }
